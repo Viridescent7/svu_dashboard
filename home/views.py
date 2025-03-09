19f0738ef68e9e018aa90efa70e385ab7fbdc613
Irfan Jalal <mindhunter@blinkenshell.org>
2025-03-09 14:06:01 +0530

    modified views.py for active and all users

--- home/views.py
+++ home/views.py
@@ -4,44 +4,29 @@ import os
 
 # Create your views here.
 
-
 def index(request):
-    # active_users = os.popen('users').read()
-    # active_users_list = []
-    # for active_user in active_users.strip().split():
-    #     if active_user not in active_users_list:
-    #         active_users_list.append(active_user)
 
-    # all_users = os.popen('cat /etc/passwd').read()
-    # all_users_list = []
-    # for user in all_users.strip().split('\n'):
-    #         if ('/home/' and ':/bin/bash') in user:
-    #                 if 'root' not in user:
-    #                         all_users_list.append(user.split(":")[0])
+    active_users = []
+    all_users = []
 
-    services = {'Dnote':'3000', 'Jellyfin':'8096', 'Jellyseer':'5055',
-            'Hacktricks':'3337', 'Hacktricks Cloud':'3377', 'KASM (Dynamic workspaces)':'8141',
-            'service7':'port7', 'service8':'port8', 'service9':'port9'}
+    # Active users 
+    tmp_users = os.popen("last -w | grep logged | awk '{print $1}'").readlines()
+    [ active_users.append(user.strip('\n')) for user in tmp_users if user.strip('\n') not in active_users ]
 
+    # All users 
+    tmp_users = os.popen('ls /home').readlines()
+    all_users = [ user.strip('\n') for user in tmp_users ]
 
-    # get_ip = os.popen("ip addr | grep 'inet 100'").read()
-    # ip_address = get_ip.strip().split()[1].split('/')[0]
+    # Running services 
+    services = {
+        'Dnote':'3000', 'Jellyfin':'8096', 'Jellyseer':'5055',
+        'Hacktricks':'3337', 'Hacktricks Cloud':'3377', 'KASM':'8141',
+        }
 
-    # return render(request, 'index.html', context={'active_users': active_users_list,
-    #                                             'all_users': all_users_list,
-    #                                             'services': services,
-    #                                             'ip_addr': ip_address})
-
-    # uncomment above lines and replace and remove below lines in this function.
-
-    ip_addr = '192.168.0.102'
-    return render(request, 'index.html', context={'active_users': [chr(i) for i in range(65, 91)], 
-                                                  'all_users': ['deathstar107', 'SaiiDen', 'Duckdarsh', 'Kabutozect', 'sgtstringplucker', 'niffler'], 
-                                                  'services': services,
-                                                  'ip_addr': ip_addr})  
-
-
-   
+    return render(request, 'index.html', context={'active_users': active_users,
+                                                'all_users': all_users,
+                                                'services': services,
+                                                'ip_addr': '127.0.0.1'})
 
 def render_sign_up(request):
     return render(request, 'sign_up.html' )
